fc5ce186dc7d4be51188657baa7557344865bfea
FateXRebirth <FateXRebirth@gmail.com>
2017-10-05 23:43:30 +0800

    Translate form prototype

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!-- <img src="./assets/logo.png">
+    <router-view></router-view> -->
+    <h1> Word Translator </h1>
+    <h5> Powered by Vue.Js </h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+
   </div>
 </template>
 
 <script>
+import TranslateForm from './components/TranslateForm';
+
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TranslateForm,
+  },
+  methods: {
+    translateText(text) {
+      alert(text);
+    }
+  }
 }
 </script>
 
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="translateForm">
+    TRANSLATE FORM
+    <form v-on:submit="formSubmit">
+        <input type="text" v-model="textToTranslate" placeholder="Enter a word...">
+        <input type="submit" value="Translate">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data() {
+      return {
+          textToTranslate: "",
+      }
+  },
+  methods: {
+      formSubmit(e) {
+          this.$emit('formSubmit', this.textToTranslate);
+          e.preventDefault();
+      }
+  }
+}
+</script>
+
+<style>
+#translate {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
